933b3be888237286cc2e1538c4029fd66caed70a
AbihaFatima <gl2987@myamu.ac.in>
2021-05-08 22:25:05 +0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Calculator from "./components/Calculator"
 
 function App() {
+  const styles ={
+    textAlign: "center",
+    fontFamily: "Lucida Console",
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Calculator />
+    <h4 style={styles}>Made with 💗 |&#169; 2021 </h4>
     </div>
   );
 }
